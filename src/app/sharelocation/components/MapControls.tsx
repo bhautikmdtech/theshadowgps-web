d66e8d77480d9717976fa4052925c6eae78a935f
bhautik.mdtech <bhautik.mdtech@gmail.com>
2025-05-15 18:36:15 +0530

    update design1

--- src/app/sharelocation/components/MapControls.tsx
+++ src/app/sharelocation/components/MapControls.tsx
@@ -183,7 +183,7 @@ const MapControls = ({ map, deviceLocation }: MapControlsProps) => {
 
   return (
     <div
-      className="fixed right-1 bottom-[88px] md:top-2 md:right-4 flex flex-col gap-2 z-10"
+      className="fixed right-2 bottom-[100px] md:top-2 md:right-4 flex flex-col gap-2 z-10"
       style={{ paddingBottom: "env(safe-area-inset-bottom)" }}
     >
       {/* Map Type Button */}
