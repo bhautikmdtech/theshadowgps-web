0c2050db2f14757c2dade5b3d2ad3cb4c66bc964
bhautik.mdtech <bhautik.mdtech@gmail.com>
2025-05-28 18:21:50 +0530

    update design

--- src/app/sharelocation/components/ShareLocationViewer.tsx
+++ src/app/sharelocation/components/ShareLocationViewer.tsx
@@ -451,7 +451,7 @@ export default function LiveTracker({
               mapRef={mapRef}
             />
             {/* MapControls will be dynamically loaded and will handle its own initialization */}
-            <div className="absolute right-2 bottom-[100px] md:bottom-[10px] md:right-4 z-[999]">
+            <div className="fixed right-2 bottom-[100px] md:bottom-[10px] md:right-4 z-[999]">
               {positions.length > 0 && (
                 <DynamicMapControls
                   map={mapRef.current}
